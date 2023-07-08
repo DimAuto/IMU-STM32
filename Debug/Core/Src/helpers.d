4f365087ddb2fb2cf48c3e5014fa4327d058ce12
kalai <kalaitzakisdim@gmail.com>
2023-07-08 23:41:54 +0300

    Removed Debug dir

--- Debug/Core/Src/helpers.d
+++
@@ -1,2 +0,0 @@
-Core/Src/helpers.o: ../Core/Src/helpers.c ../Core/Src/helpers.h
-../Core/Src/helpers.h:
--- Debug/Core/Src/ring_buffer.d
+++
@@ -1,3 +0,0 @@
-Core/Src/ring_buffer.o: ../Core/Src/ring_buffer.c \
- ../Core/Src/ring_buffer.h
-../Core/Src/ring_buffer.h:
